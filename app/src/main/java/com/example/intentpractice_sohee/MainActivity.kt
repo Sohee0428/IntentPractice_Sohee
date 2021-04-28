6ce4b09124ceb9aa08f97ab087eb7661ad6f394f
Sohee <sohee3480@gmail.com>
2021-04-28 21:46:38 +0900

    VIEW 액션 예제  - 인터넷 링크  - 플레이스토어 상세화면 링크

--- app/src/main/java/com/example/intentpractice_sohee/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice_sohee/MainActivity.kt
@@ -107,6 +107,29 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+
+
+        naverWebBtn.setOnClickListener {
+
+            val myUri = Uri.parse("http://naver.com")
+
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+
+            startActivity(myIntent)
+        }
+
+
+
+        kakaoStoreBtn.setOnClickListener {
+
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+
+            startActivity(myIntent)
+        }
+
     }
 
 
