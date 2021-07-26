c278a93d16acf72ab29cc20e61c8e058bf7d93b2
Sohee <sohee3480@gmail.com>
2021-07-26 17:42:08 +0900

    닉네임 변경을 위한 데이터 들고오기 작업

--- app/src/main/java/com/example/intentpractice_sohee/test/NicknameEditActivity2.kt
+++ app/src/main/java/com/example/intentpractice_sohee/test/NicknameEditActivity2.kt
@@ -1,5 +1,6 @@
 package com.example.intentpractice_sohee.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.intentpractice_sohee.R
@@ -10,6 +11,14 @@ class NicknameEditActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nickname_edit2)
 
-        okBtn2.setOnClickListener {  }
+        okBtn2.setOnClickListener {
+
+            val inputNewNickname = newNicknameEdt2.text.toString()
+            val resultIntent = Intent()
+
+            resultIntent.putExtra("nickname", inputNewNickname)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }
